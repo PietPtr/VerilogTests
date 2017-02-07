bc9a2c22d5a7307aa857c08ab62dea15b7aca249
Pieter Staal <pieterstaal@outlook.com>
2017-02-07 18:37:44 +0100

    Using a module in another module
    http://electronics.stackexchange.com/questions/64752/verilog-including-one-module-in-another-module
    http://www.asic-world.com/verilog/syntax2.html

--- test.v
+++ test.v
@@ -1,4 +1,17 @@
-module test (input a, b, output y);
-  wire x = a ~& b;
-  assign y = (a ~& x) ~& (x ~& b);
+// iverilog -o test test.v test_tb.v && vvp test
+
+module test (input a, b, output out);
+    wire [0:0] y;
+    my_xor u0 (
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    assign out = ~y;
+endmodule
+
+module my_xor (input a, b, output y);
+    wire x = a ~& b;
+    assign y = (a ~& x) ~& (x ~& b);
 endmodule
--- test_tb.v
+++ test_tb.v
@@ -1,30 +1,30 @@
 module tb;
     reg [0:0] a, b;
-    wire [0:0] out;
+    wire [0:0] r;
 
     test DUT (
         .a(a),
         .b(b),
-        .y(y)
+        .out(r)
     );
 
     initial begin
         #20
         a = 0;
         b = 0;
-        $monitor("%h %h | %h", a, b, y);
+        $monitor("%h %h | %h", a, b, r);
         #20
         a = 0;
         b = 1;
-        $monitor("%h %h | %h", a, b, y);
+        $monitor("%h %h | %h", a, b, r);
         #20
         a = 1;
         b = 0;
-        $monitor("%h %h | %h", a, b, y);
+        $monitor("%h %h | %h", a, b, r);
         #20
         a = 1;
         b = 1;
-        $monitor("%h %h | %h", a, b, y);
+        $monitor("%h %h | %h", a, b, r);
         #20
         $finish;
     end
