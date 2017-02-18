19492d3b8dd431fe3916e700c41b644909e08398
Pieter Staal <pieterstaal@outlook.com>
2017-02-18 11:15:42 +0100

    Full adder

--- test.v
+++ test.v
@@ -2,22 +2,41 @@
 
 module test (input a, b, output out);
     wire [0:0] y;
-    my_xor u0 (
-        .a(a),
-        .b(b),
-        .y(y)
-    );
 
     assign out = y;
 endmodule
 
-module my_xor (input a, b, output y);
-    wire x = a ~& b;
-    assign y = (a ~& x) ~& (x ~& b);
-endmodule
-
-module halfadder (input a, b, output carry, sum);
+module halfadder (input a, b, output carry, sum, oa, ob);
+    assign a = oa;
+    assign b = ob;
     assign carry = a & b;
     assign sum = a ^ b;
 endmodule
 
+module fulladder (input a, b, c, output carry, sum);
+    wire carry0;
+    wire carry1;
+    wire sum0;
+    wire sum1;
+
+    halfadder h0 (
+        .a(a),a
+        .b(b),
+        .carry(carry0),
+        .sum(sum0)
+    );
+
+    halfadder h1 (
+        .a(sum0),
+        .b(c),
+        .carry(carry1),
+        .sum(sum1)
+    );
+
+    assign carry = carry0 | carry1;
+    assign sum = sum1;
+endmodule
+
+module alu (input a[4], b[4], output r[4]);
+    assign r = a;
+endmodule
