134be352be4bb34f83ea9fe2acd23587e69e8e4f
Pieter Staal <pieterstaal@outlook.com>
2017-03-03 23:43:02 +0100

    Add 16 address 4 bit memory

---
+++ mem/mem.v
@@ -0,0 +1,15 @@
+// 4 bit RAM module, 16 addresses storing 4 bit values.
+module mem (input load, clock, input[3:0] in, addr, output o1, o2, o3, o4, o5);
+    reg[3:0] memory[0:15];
+
+    always @ (posedge clock)
+    if (load) begin
+        memory[addr] <= in;
+    end
+
+    assign o1 = memory[addr][0];
+    assign o2 = memory[addr][1];
+    assign o3 = memory[addr][2];
+    assign o4 = memory[addr][3];
+    assign o5 = load;
+endmodule
