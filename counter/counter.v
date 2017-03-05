a7f4e24f4f537e1acad8542fdf8d60340799d8e8
Pieter Staal <pieterstaal@outlook.com>
2017-03-05 15:19:46 +0100

    Add program counter

--- counter/counter.v
+++ counter/counter.v
@@ -3,19 +3,18 @@ module counter (input inc, load, reset, clock, input[3:0] in, output o1, o2, o3,
     reg[3:0] out;
 
     always @ (posedge clock)
-    if (load) begin
-        out <= in;
-    end
-    else if (inc) begin
-        out <= out + 1;
-    end
-    else if (reset) begin
-        out <= 0;
+    begin
+        if (load)
+            out <= in;
+        if (inc)
+            out <= out + 1;
+        if (reset)
+            out <= 0;
     end
 
     assign o1 = out[0];
     assign o2 = out[1];
     assign o3 = out[2];
     assign o4 = out[3];
-    assign o5 = load;
+    assign o5 = clock;
 endmodule
