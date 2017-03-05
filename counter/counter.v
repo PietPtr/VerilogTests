c93a48ed21767f0b6f0d3b7ed10eb16a217af6da
Pieter Staal <pieterstaal@outlook.com>
2017-03-05 16:06:59 +0100

    Add slow_clock

--- counter/counter.v
+++ counter/counter.v
@@ -1,8 +1,21 @@
 // 4 bit RAM module, 16 addresses storing 4 bit values.
-module counter (input inc, load, reset, clock, input[3:0] in, output o1, o2, o3, o4, o5);
+module counter (input inc, load, clock, reset, input[3:0] in, output o1, o2, o3, o4, o5);
     reg[3:0] out;
+    reg slow_clock = 1;
+    reg[22:0] clock_counter = 1;
 
     always @ (posedge clock)
+    begin
+        if (clock_counter > 1200000) begin
+            clock_counter = 0;
+            slow_clock <= ~slow_clock;
+            end
+        else begin
+            clock_counter <= clock_counter + 1;
+            end
+    end
+
+    always @ (posedge slow_clock)
     begin
         if (load)
             out <= in;
@@ -16,5 +29,5 @@ module counter (input inc, load, reset, clock, input[3:0] in, output o1, o2, o3,
     assign o2 = out[1];
     assign o3 = out[2];
     assign o4 = out[3];
-    assign o5 = clock;
+    assign o5 = slow_clock;
 endmodule
